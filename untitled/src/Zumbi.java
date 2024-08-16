a1eedf4cba6e1a7992effbb1c16357e1e70aa5ff
Vinicius145 <vinicius.maciel@get.inatel.br>
2024-08-15 21:51:40 -0300

    Update Zumbi.java

--- untitled/src/Zumbi.java
+++ untitled/src/Zumbi.java
@@ -2,6 +2,8 @@ public class Zumbi {
         int idade;
         String nome;
         public void seAlimentar(){
-                System.out.println("zombi esta se alimentando");
+                System.out.println(nome + "esta se alimentando");//mostra que o ou os zumbi(S) criado na funcao main 
+                idade = 10;
+                
         }
 }
