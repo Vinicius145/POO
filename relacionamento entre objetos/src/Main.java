d7b4c4bd293f53cf41312b731601799553f4c0d9
Vinicius145 <vinicius.maciel@get.inatel.br>
2024-08-19 20:39:26 -0300

    introducao a composicao
    introducao a composicao

---
+++ relacionamento entre objetos/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Empresa.dep.nome);//mostra o nome vinculado ao departamento sem precisar declarar o departamento na main
+    }
+}
